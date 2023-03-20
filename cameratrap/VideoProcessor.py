d55df1d652dff5efd58261dcf218ff63ce92b7eb
Daniel Cane <daniel.cane@modmed.com>
2023-03-20 16:16:56 -0400

    looping through images and preforming inference working generated output images need to save to database

--- cameratrap/VideoProcessor.py
+++ cameratrap/VideoProcessor.py
@@ -7,8 +7,26 @@ import subprocess
 from ultralytics import YOLO
 from PIL import Image
 from numpy import asarray
+from ultralytics.yolo.utils.plotting import Annotator
+
+def create_video_frames_dir(parent_dir, new_dir):
+    path_full_dir = os.path.join(parent_dir, new_dir)
+    is_exist = os.path.exists(path_full_dir)
+    if not is_exist:
+        os.mkdir(path_full_dir)
+    return path_full_dir
+
 
 class VideoProcessor:
+    FPS = "2"  # change the video FPS processing rate
+    FRAMES_DIR = "_frames"  # subdir for image frames of each video
+    ANNOTATION_DIR = "_annotations"  # subdir of annotated frames
+
+    path_to_video = ""
+    video_filename = ""
+    parent_dir = ""
+    path_frames_dir = ""
+    path_annotation_dir = ""
 
     def __init__(self, videoFile):
         self.videoFile = videoFile
@@ -22,61 +40,40 @@ class VideoProcessor:
     def convertVideoToImages(self):
         logging.debug("- Converting Video to Images")
 
-        path_to_video = self.videoFile.document.path
-        video_filename = ntpath.basename(path_to_video)
-        parent_dir = ntpath.dirname(path_to_video)
-        output_directory = video_filename + "_frames"
-
-        # Path
-        path_output_dir = self.create_video_frames_dir(output_directory, parent_dir)
-
-        logging.debug("- Path: "+ path_to_video)
-        logging.debug("- Filename: "+ video_filename)
-        logging.debug("- Output Dir: "+ path_output_dir)
+        # configure all the directories
+        self.configureDirectories()
 
         # convert video to images
-        self.process_video_using_ffmpeg(path_output_dir, path_to_video)
+        self.process_video_using_ffmpeg()
 
         # Load a model
         model = YOLO('model/v10best.pt')  # load a pretrained model (recommended for training)
 
         # run inference on all images
-        for filename in sorted(glob.glob(os.path.join(path_output_dir, '*.jpg'))):
+        for filename in sorted(glob.glob(os.path.join(self.path_frames_dir, '*.jpg'))):
             img = Image.open(filename)
 
             # do your stuff
-            logging.debug ("- Running inference on image "+ filename)
-            self.run_inference(model, img)
+            logging.debug("- Running inference on image " + filename)
+            self.run_inference(filename, model, img)
 
-    def run_inference(self, model, img):
-        numpydata = asarray(img)
+    def configureDirectories(self):
+        self.path_to_video = self.videoFile.document.path
+        self.video_filename = ntpath.basename(self.path_to_video)
+        self.parent_dir = ntpath.dirname(self.path_to_video)
+        # Paths
+        frames_working_dir = self.video_filename + self.FRAMES_DIR
+        annotations_working_dir = self.video_filename + self.ANNOTATION_DIR
+        self.path_frames_dir = create_video_frames_dir(self.parent_dir, frames_working_dir)
+        self.path_annotation_dir = create_video_frames_dir(self.parent_dir, annotations_working_dir)
+        logging.debug("- Path: " + self.path_to_video)
+        logging.debug("- Filename: " + self.video_filename)
+        logging.debug("- Frames Dir: " + self.path_frames_dir)
+        logging.debug("- Annotation Dir: " + self.path_annotation_dir)
 
-        results = model(numpydata)  # generator of Results objects
-        logging.debug("-- Inference Complete ")
-        logging.debug(results)
-        for r in results:
-            boxes = r.boxes  # Boxes object for bbox outputs
-            # masks = r.masks  # Masks object for segmenation masks outputs
-            # probs = r.probs  # Class probabilities for classification outputs
-            if boxes is not None:
-                logging.debug("---- boxes: " )
-                logging.debug(boxes)
-
-                boxXY = boxes.xywh # box with xywh format, (N, 4)
-                boxConf = boxes.conf # confidence score, (N, 1)
-
-                logging.debug("-- boxXY ")
-                logging.debug(boxXY)
-                logging.debug("-- boxConf ")
-                logging.debug(boxConf)
-
-        res_plotted = results[0].plot()
-        res_plotted.save(results[0].path + "testOut.jpg")
-
-
-    def process_video_using_ffmpeg(self, path_output_dir, path_to_video):
+    def process_video_using_ffmpeg(self):
         # ffmpeg -i input.mp4 -vf fps=15 frame%d.png
-        ffmpeg_cmd = "/opt/homebrew/bin/ffmpeg -i " + path_to_video + " -vf fps=15 " + path_output_dir + "/frame%d.jpg"
+        ffmpeg_cmd = "/opt/homebrew/bin/ffmpeg -i " + self.path_to_video + " -vf fps=" + self.FPS + " " + self.path_frames_dir + "/frame%d.jpg"
         logging.debug("- Running ffmpeg command: " + ffmpeg_cmd)
         ffmpeg = subprocess.Popen(ffmpeg_cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
         out, err = ffmpeg.communicate()
@@ -86,12 +83,31 @@ class VideoProcessor:
         else:
             print("- SUCCESS: FFmpeg Script Ran Successfully")
 
-    def create_video_frames_dir(self, output_directory, parent_dir):
-        path_output_dir = os.path.join(parent_dir, output_directory)
-        isExist = os.path.exists(path_output_dir)
-        if not isExist:
-            os.mkdir(path_output_dir)
-        return path_output_dir
+    def run_inference(self, filename, model, img):
+        img_as_numpy = asarray(img)
 
+        results = model(img_as_numpy)  # generator of Results objects
+        logging.debug("-- Inference Complete ")
+        # logging.debug(results)
+        for r in results:
+            boxes = r.boxes  # Boxes object for bbox outputs
+            # masks = r.masks  # Masks object for segmenation masks outputs
+            # probs = r.probs  # Class probabilities for classification outputs
+            annotator = Annotator(img_as_numpy, line_width=1)
+            if boxes is not None:
+                for box in boxes:
+                    b = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
+                    c = box.cls
+                    conf = box.conf[0]
+                    # logging.debug("-=-=-=-=-=-=--=-=-=-=-=-=-=-")
+                    # logging.debug(conf)
+                    # logging.debug("-=-=-=-=-=-=--=-=-=-=-=-=-=-")
+                    class_with_conf = '{label} - {con:.2f}%'.format(label=model.names[int(c)], con=conf * 100)
+                    annotator.box_label(b, class_with_conf, color=(255, 0, 0))
 
+        img_annotated = annotator.result()
+        image_final = Image.fromarray(img_annotated)
 
+        video_filename = ntpath.basename(filename)
+        annotated_path_to_output = self.path_annotation_dir + "/" + video_filename
+        image_final.save(annotated_path_to_output)
