2c3a0b773f3f8613c9657e13bb46db5ce7d0fe56
Daniel Cane <daniel.cane@modmed.com>
2023-05-01 13:37:42 -0400

    Adding test files for OCR and updating test cases.

--- cameratrap/OCRUtils.py
+++ cameratrap/OCRUtils.py
@@ -1,67 +1,86 @@
+from datetime import datetime
+
 import cv2
 import pytesseract
 import re
 import dateparser
 import logging
 
-# Validate date
-date_extract_pattern = "[0-9]{1,2}.[0-9]{1,2}.[0-9]{4}" #MM/DD/YYYY
-time_pattern_1 = "\\b([0-5][0-9]:[0-5][0-9]\\w{2})\\b" #01:22PM
-time_extract_2 = "\\b([0-5][0-9]:[0-5][0-9]:[0-5][0-9])\\b" #14:22:56
-time_all_patterns = time_pattern_1 +"|"+ time_extract_2
+class OCRUtils:
 
-pattern_date = re.compile(date_extract_pattern, re.IGNORECASE)
-pattern_time = re.compile(time_all_patterns, re.IGNORECASE)
+    def __init__(self, image):
+        self.imageToParse = image
 
-# Direct from the command line - the whole image
-#print(pytesseract.image_to_string('../media/videos/sit1/ct1/TEST.mp4_frames/time.jpg', lang="eng", config="--psm 7"))
+    def extractDateTime(self):
+        logging.debug("-=-=-=-=-=-=-=-=-=-=-=-= start extractDateTime -=-=-=-=-=-=-=-=-=-=-=-=")
+        dt = self.processImage()
+        logging.debug("-=-=-=-=-=-=-=-=-=-=-=-=  end extractDateTime  -=-=-=-=-=-=-=-=-=-=-=-=")
+        return dt
 
-img_cv = cv2.imread(r'../media/videos/sit1/ct1/TEST.mp4_frames/frame1.jpg')
-#img_cv = cv2.imread(r'../media/videos/444/333/14sCameraType2.mov_annotations/frame8.jpg')
-image_bytes = cv2.imencode('.jpg', img_cv)[1].tobytes()
+    def processImage(self):
+        # Validate date
+        date_extract_pattern = "[0-9]{1,2}.[0-9]{1,2}.[0-9]{4}" #MM/DD/YYYY
+        time_pattern_1 = "\\b([0-5][0-9]:[0-5][0-9]\\w{2})\\b" #01:22PM
+        time_extract_2 = "\\b([0-5][0-9]:[0-5][0-9]:[0-5][0-9])\\b" #14:22:56
+        time_all_patterns = time_pattern_1 +"|"+ time_extract_2
 
-# By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
-# we need to convert from BGR to RGB format/mode:
-img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+        pattern_date = re.compile(date_extract_pattern, re.IGNORECASE)
+        pattern_time = re.compile(time_all_patterns, re.IGNORECASE)
 
-dimensions = img_rgb.shape
-height = img_rgb.shape[0]
-width = img_rgb.shape[1]
-#print(f"height: %2d" % height);
-#print(f"width: %2d " % width);
+        # Direct from the command line - the whole image
+        #print(pytesseract.image_to_string('../media/videos/sit1/ct1/TEST.mp4_frames/time.jpg', lang="eng", config="--psm 7"))
 
-#subimage = image[Y_start : Y_end, X_start : X_end].
-crop_img = img_rgb[height-40:height, 100:width]
+        #img_cv = cv2.imread(r'../media/videos/sit1/ct1/TEST.mp4_frames/frame1.jpg')
+        #img_cv = cv2.imread(r'../media/videos/444/333/14sCameraType2.mov_annotations/frame8.jpg')
+        #image_bytes = cv2.imencode('.jpg', img_cv)[1].tobytes()
 
-#cv2.imshow("orig", crop_img)
-#cv2.waitKey(0)
+        # By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
+        # we need to convert from BGR to RGB format/mode:
+        #img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+        img_rgb = cv2.cvtColor(self.imageToParse, cv2.COLOR_BGR2RGB)
 
-ocr_string = pytesseract.image_to_string(crop_img, lang="eng", config="--psm 7")
-logging.debug(ocr_string)
+        dimensions = img_rgb.shape
+        height = img_rgb.shape[0]
+        width = img_rgb.shape[1]
+        #print(f"height: %2d" % height);
+        #print(f"width: %2d " % width);
 
-search_date = pattern_date.search(ocr_string) # Returns Match object
-date_as_string = ''
-time_as_string = ''
+        #subimage = image[Y_start : Y_end, X_start : X_end].
+        crop_img = img_rgb[height-44:height, 100:width]
 
-#print(search_date)
-if search_date is not None:
-    date_as_string = search_date.group()
-    logging.debug('Date is: {0}'.format(date_as_string))
+        #cv2.imwrite("/Users/daniel.cane/PycharmProjects/mysite/media/last_ocr_crop.jpg", crop_img)
+        #cv2.imshow("orig", crop_img)
+        #cv2.waitKey(0)
 
-search_time = pattern_time.search(ocr_string) # Returns Match object
-#print(search_time)
-if search_time is not None:
-    time_as_string = search_time.group()
-    logging.debug('Time is: {0}'.format(time_as_string))
+        ocr_string = pytesseract.image_to_string(crop_img, lang="eng", config="--psm 7")
+        logging.debug(ocr_string)
 
-date_time_string = date_as_string +" "+ time_as_string
-dt = dateparser.parse(date_time_string)
-logging.debug(dt)
+        search_date = pattern_date.search(ocr_string) # Returns Match object
+        date_as_string = ''
+        time_as_string = ''
 
-# OR
-#img_rgb = Image.frombytes('RGB', img_cv.shape[:2], img_cv, 'raw', 'BGR', 0, 0)
-#print(pytesseract.image_to_string(img_rgb))
+        #print(search_date)
+        if search_date is not None:
+            date_as_string = search_date.group()
+            logging.debug('Date is: {0}'.format(date_as_string))
 
+        search_time = pattern_time.search(ocr_string) # Returns Match object
+        #print(search_time)
+        if search_time is not None:
+            time_as_string = search_time.group()
+            logging.debug('Time is: {0}'.format(time_as_string))
+
+        date_time_string = date_as_string +" "+ time_as_string +" UTC"
+        dt: datetime | None = dateparser.parse(date_time_string)
+        logging.debug(dt)
+
+        return dt
+
+        # OR
+        #img_rgb = Image.frombytes('RGB', img_cv.shape[:2], img_cv, 'raw', 'BGR', 0, 0)
+        #print(pytesseract.image_to_string(img_rgb))
+
+    pass
 
 
 
--- cameratrap/tests.py
+++ cameratrap/tests.py
@@ -1,3 +1,29 @@
+import dateparser
+from PIL import Image
 from django.test import TestCase
+from numpy import asarray
+from cameratrap.OCRUtils import OCRUtils
 
-# Create your tests here.
+
+class OCRUtilTests(TestCase):
+
+
+    def test_ocr_bushnell(self):
+        img = Image.open(r'./cameratrap/testdata/bushnell.jpg')
+        img_as_numpy = asarray(img)
+        ocr = OCRUtils(image=img_as_numpy)
+        dt = ocr.extractDateTime()
+        dt_in_image = dateparser.parse('2022-05-04 16:08:36+00:00')
+        self.assertEqual(dt.date(), dt_in_image.date())
+        self.assertEqual(dt.time(), dt_in_image.time())
+        print(dt)
+
+    def test_ocr_deer(self):
+        img = Image.open(r'./cameratrap/testdata/deer.jpg')
+        img_as_numpy = asarray(img)
+        ocr = OCRUtils(image=img_as_numpy)
+        dt = ocr.extractDateTime()
+        dt_in_image = dateparser.parse('2022-03-04 07:18:00+00:00')
+        self.assertEqual(dt.date(), dt_in_image.date())
+        self.assertEqual(dt.time(), dt_in_image.time())
+        print(dt)
